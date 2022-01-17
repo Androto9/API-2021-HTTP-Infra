1c11cd877898b9a08ac6ff30beb1da7aca69b98d
antoineuldry <antoine.uldry@heig-vd.ch>
2022-01-17 13:26:07 +0100

    Update step 5

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,9 +4,9 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){ res.send("Salut");});
+app.get('/zoo/animals', function(req, res){ res.send(displayZoo());});
 app.get('/zoo', function(req, res){ res.send("Welcome to the zoo");});
-app.get('/zoo/animals' , function(req, res){ res.send(displayZoo());});
+app.get('/', function(req, res){ res.send("Salut");});
 
 app.listen(3000, function(){ console.log('Accepting HTTP requests on port 3000.');});
 
